013d6e5c16d8aa68b68f14e45e7777edab6af57c
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-05-11 09:38:28 +0200

    Create project

---
+++ src/main/java/com/romanstetsiuk/final_app_sda/FinalAppSdaApplication.java
@@ -0,0 +1,13 @@
+package com.romanstetsiuk.final_app_sda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAppSdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalAppSdaApplication.class, args);
+    }
+
+}
