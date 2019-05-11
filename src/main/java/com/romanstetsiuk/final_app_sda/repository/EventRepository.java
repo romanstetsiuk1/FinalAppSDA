fe2518ecf6d8f93a0e0d623abef6bbbd42524c3b
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-05-11 12:09:50 +0200

    Create repository by event

---
+++ src/main/java/com/romanstetsiuk/final_app_sda/repository/EventRepository.java
@@ -0,0 +1,8 @@
+package com.romanstetsiuk.final_app_sda.repository;
+
+import com.romanstetsiuk.final_app_sda.entity.Event;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventRepository extends CrudRepository<Event, Integer> {
+
+}
