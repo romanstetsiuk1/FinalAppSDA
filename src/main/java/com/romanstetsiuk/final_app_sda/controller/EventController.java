2dd6cac137a848b4f32bb89c9ecfbdee391c27bc
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-05-12 13:53:47 +0200

    Modification pom.xml EventController application properies

--- src/main/java/com/romanstetsiuk/final_app_sda/controller/EventController.java
+++ src/main/java/com/romanstetsiuk/final_app_sda/controller/EventController.java
@@ -3,11 +3,12 @@ package com.romanstetsiuk.final_app_sda.controller;
 import com.romanstetsiuk.final_app_sda.entity.Event;
 import com.romanstetsiuk.final_app_sda.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "/events")
 public class EventController {
 
@@ -21,8 +22,12 @@ public class EventController {
 
     @PostMapping
     public Event addEvent(@RequestBody Event event) {
-
         return eventRepository.save(event);
+    }
+
+    @GetMapping(path = "/test")
+    String showEvents() {
+        return "events";
     }
 
 }
