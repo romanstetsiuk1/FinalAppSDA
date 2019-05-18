af0f3a0a5e1475275309cd42aac60b60bb8860c6
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-05-18 12:46:55 +0200

    correct mistake

---
+++ src/main/java/com/romanstetsiuk/final_app_sda/controller/UserConroller.java
@@ -0,0 +1,20 @@
+package com.romanstetsiuk.final_app_sda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class UserConroller {
+
+    @RequestMapping(value = "/user/registration", method = RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+
+
+}
