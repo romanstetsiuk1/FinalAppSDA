17d9f87f24fbe276397878b2413feb41ef1b8828
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-05-28 19:53:33 +0200

    create new branch

--- src/main/java/com/romanstetsiuk/final_app_sda/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.romanstetsiuk.final_app_sda.repository;
-
-import com.romanstetsiuk.final_app_sda.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
